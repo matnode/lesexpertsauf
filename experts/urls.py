a1534aae2c52739e18ddf3deeb75c971406c55e2
Martial <martial.nodem@auf.org>
2015-08-17 17:23:40 +0100

    intégration du design web

--- experts/urls.py
+++ experts/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls.defaults import *
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('experts.views',
-    (r'^login/$', 'index'),
-    (r'^inscription/$', 'inscription'),
-)
+
