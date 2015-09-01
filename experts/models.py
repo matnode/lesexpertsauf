d369a458c0a6f93a58f1a8ca28e45f57db493453
Martial <martial.nodem@auf.org>
2015-09-01 17:04:00 +0100

    cohabitation entre les experts et etablissement ajout du type de compte

--- experts/models.py
+++ experts/models.py
@@ -4,6 +4,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Typecompte (models.Model):
+       user =  models.OneToOneField(User)
+       typedecompte = models.CharField(max_length=200)
+       
+       def __unicode__(self):
+          return self.typedecompte
+
 class Human(models.Model):
     user = models.OneToOneField(User)
     nom = models.CharField(max_length=200) 
@@ -117,5 +124,33 @@ class Langue(models.Model):
     def __unicode__(self):
           return self.nom
     def __unicode__(self):
-          return self.niveau 
-             
+          return self.niveau
+
+class Entreprise(models.Model):
+    user = models.OneToOneField(User)
+    nom = models.TextField()
+    description = models.TextField()
+    datedefondation = models.CharField(max_length=200)
+    codepostale = models.CharField(max_length=200)
+    ville = models.CharField(max_length=200)
+    taille = models.CharField(max_length=200)
+    siteweb = models.TextField()
+    telephone = models.CharField(max_length=255)
+    adresse = models.CharField(max_length=255)
+    activite = models.TextField()
+
+class Offre(models.Model):
+    entreprise = models.ForeignKey(Entreprise)
+    reference = models.CharField(max_length=200)
+    intitule = models.TextField()
+    ville = models.CharField(max_length=200)
+    region = models.CharField(max_length=200)
+    secteuractivite = models.TextField()
+    mission = models.TextField()
+    typedecontra = models.CharField(max_length=200)
+    periodaffichage = models.CharField(max_length=255)
+    grillesalaire = models.TextField(max_length=255)
+    datelimite = models.CharField(max_length=255)
+    description= models.TextField()
+    dureecontrat = models.CharField(max_length=200)
+    dateprisefonction = models.CharField(max_length=200)
